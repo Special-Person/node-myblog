e4ee394c205a82a53b066d55b7c93794680b584e
Special-Person <805321556@qq.com>
2021-08-08 18:35:11 +0800

    update

--- dao/BaseDao.js
+++ dao/BaseDao.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 // 创建连接 
 const createConnection = () => mysql.createConnection({
-    host: "127.0.0.1",
+    host: "192.168.123.78",
     port: "3306",
     user: "root",
     password: "qwer1234",
